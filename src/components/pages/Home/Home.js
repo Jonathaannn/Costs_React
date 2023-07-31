2b4db418933b22e892ed5b9305f4663568ff85e5
Jonathaannn <jonathanjog237@gmail.com>
2023-07-31 20:23:48 -0300

    Criando a página de formulário

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,16 +1,20 @@
 import image from "../../../img/savings.svg"
+import Container from "../../layouts/Container/Container"
 import LinkButton from "../../layouts/LinkButton/LinkButton"
 import styles from "./Home.module.css"
 
 function Home () {
     return (
+    <Container customClass="min-height">
         <section className={styles.home_container}>
             <h1>Bem-vindo ao <span>Costs</span></h1>
             <p>Comece a gerenciar os seus projetos agora mesmo!</p>
             <LinkButton to="/newproject" text="Criar Projeto"/>
             <img src={image} alt="Costs"/>
         </section>
+    </Container>
     )
 }
 
 export default Home
+
--- src/components/pages/NewProject/NewProject.js
+++ src/components/pages/NewProject/NewProject.js
@@ -1,14 +1,16 @@
 import Container from "../../layouts/Container/Container";
+import ProjectForm from "../../project/ProjectForm";
 import styles from "./NewProject.module.css"
 
 function NewProject () {
     return (
-        <>
-        <Container customClass="min-height">
-            <h1>New Project</h1>
-            <p>Conteúdo da página</p>
-        </Container>
-        </>
+    <Container customClass="min-height">
+        <div className={styles.newproject_container}>
+            <h1>Criar Projeto</h1>
+            <p>Crie seu projeto para depois adicionar os serviços</p>
+            <ProjectForm />
+        </div>
+    </Container>
     )
 }
 
