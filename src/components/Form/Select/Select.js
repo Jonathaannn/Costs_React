0419a60c5647dfdf41eba3a661d27af733e334d1
Jonathaannn <jonathanjog237@gmail.com>
2023-08-01 15:30:42 -0300

    Criando e conectando a API da aplicação

--- src/components/Form/Select/Select.js
+++ src/components/Form/Select/Select.js
@@ -1,14 +1,21 @@
-import styles from "./Select.module.css"
+import styles from "./Select.module.css";
 
-function Select ({text, name, options, handleOnChange, value}) {
-    return (
-        <div className={styles.form_control}>
-            <label htmlFor={name}>{text}:</label>
-            <select name={name} id={name}>
-                <option>Selecione uma opção</option>
-            </select>
-        </div>
-    )
+function Select({ text, name, options, handleOnChange, value }) {
+  return (
+    <div className={styles.form_control}>
+      <label htmlFor={name}>{text}:</label>
+      <select name={name} id={name}>
+        <option selected disabled>
+          Selecione uma opção
+        </option>
+        {options.map((item) => (
+          <option value={item.id} key={item.id}>
+            {item.name}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
 }
 
-export default Select
+export default Select;
--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -1,17 +1,47 @@
-import Input from "../Form/Input/Input"
-import Select from "../Form/Select/Select"
-import SubmitButton from "../Form/SubmitButton/SubmitButton"
-import styles from "./ProjectForm.module.css"
+import { useState } from "react";
+import Input from "../Form/Input/Input";
+import Select from "../Form/Select/Select";
+import SubmitButton from "../Form/SubmitButton/SubmitButton";
+import styles from "./ProjectForm.module.css";
+import { useEffect } from "react";
 
-function ProjectForm ({buttonText}) {
-    return (
-        <form className={styles.form}>
-            <Input type="text" text="Nome do projeto" name="name" placeholder="Insira o nome do projeto"/>
-            <Input type="number" text="Orçamento do projeto" name="budget" placeholder="Insira o orçamento total"/>
-            <Select name="category_id" text="Selecione a cetegoria"/>
-            <SubmitButton text={buttonText}/>
-        </form>
-    )
+function ProjectForm({ buttonText }) {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/category", {
+      method: "GET",
+      headers: {
+        "Content-type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => setCategories(data))
+      .catch((error) => console.log(error));
+  }, []);
+
+  return (
+    <form className={styles.form}>
+      <Input
+        type="text"
+        text="Nome do projeto"
+        name="name"
+        placeholder="Insira o nome do projeto"
+      />
+      <Input
+        type="number"
+        text="Orçamento do projeto"
+        name="budget"
+        placeholder="Insira o orçamento total"
+      />
+      <Select
+        name="category_id"
+        text="Selecione a cetegoria"
+        options={categories}
+      />
+      <SubmitButton text={buttonText} />
+    </form>
+  );
 }
 
-export default ProjectForm
+export default ProjectForm;
