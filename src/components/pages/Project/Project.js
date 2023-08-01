02695232e900e755600a4d56c96c353face6414b
Jonathaannn <jonathanjog237@gmail.com>
2023-08-01 18:56:12 -0300

    Estilizando a dashboard dos projetos

--- src/components/pages/Project/Project.js
+++ src/components/pages/Project/Project.js
@@ -2,6 +2,7 @@ import { useLocation } from "react-router-dom";
 import Container from "../../layouts/Container/Container";
 import Message from "../../layouts/Message/Message";
 import styles from "./Project.module.css";
+import LinkButton from "../../layouts/LinkButton/LinkButton";
 
 function Project() {
   const location = useLocation();
@@ -13,9 +14,15 @@ function Project() {
   return (
     <>
       <Container customClass="min-height">
-        <div>
-          <h1>Projects</h1>
+        <div className={styles.project_container}>
+          <div className={styles.title_container}>
+            <h1>Projects</h1>
+            <LinkButton to="/newproject" text="Criar projeto" />
+          </div>
           {message && <Message type="succes" text={message} />}
+          <Container customClass="start">
+            <p>Projetos...</p>
+          </Container>
         </div>
       </Container>
     </>
